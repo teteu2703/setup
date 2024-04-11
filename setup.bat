cc507ab6793b9f3608de196ed9f893da5be63602
teteugomes17 <t3t3u0327@gmail.com>
2024-04-11 11:33:15 -0300

    setup feito e testado

---
+++ setup.bat
@@ -0,0 +1,8 @@
+git config --global user.name "teteugomes17"
+git config --global user.email "t3t3u0327@gmail.com"
+
+cd C:\Users\MAUA-MERCEDESVALENTI
+
+git clone https://github.com/teteu2703/Caesar-Cipher.gi
+cd Caesar-Cipher 
+code 
