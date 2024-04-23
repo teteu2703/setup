360d842ddf999ff0918d183315f23386bebc5489
teteugomes17 <t3t3u0327@gmail.com>
2024-04-23 09:19:33 -0300

    arruma endereço

--- setup.bat
+++ setup.bat
@@ -3,6 +3,6 @@ git config --global user.email "t3t3u0327@gmail.com"
 
 cd C:\Users\MAUA-MERCEDESVALENTI
 
-git clone https://github.com/teteu2703/Caesar-Cipher.gi
+git clone https://github.com/teteu2703/Caesar-Cipher.git
 cd Caesar-Cipher 
 code 
